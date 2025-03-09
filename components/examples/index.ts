136e9f41802e206baed66cb6676529f4c05f5af7
winches <329487092@qq.com>
2025-03-09 13:22:31 +0800

    docs: support tailwindcss v4

--- components/examples/index.ts
+++ components/examples/index.ts
@@ -1,5 +1,4 @@
 export { default as SlotsExample } from './slots-example';
-export { default as ResponsiveExample } from './responsive-example';
 export { default as SlotsVariantsExample } from './slots-variants-example';
 export { default as CompoundSlotsExample } from './compound-slots-example';
 export { default as SlotsCompoundVariantsExample } from './slots-compound-variants-example';
